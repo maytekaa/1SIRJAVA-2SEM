45eabed44ff5982f041db8a948df5cc864000e62
Laboratório FIAP <labsfiap@fiap.com.br>
2026-08-03 22:34:22 -0300

    first commit

---
+++ src/Exercicio01_v2.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Exercicio01_v2 {
+
+    static void main() {
+        Scanner sc = new Scanner(System.in);
+        int valor;
+        int total = 0;
+
+        System.out.println("Informe um valor inteiro e positivo: ");
+        valor = sc.nextInt();
+
+        if (valor < 2) {
+            System.out.println("O valor deve ser maior ou igual a 2.");
+        }
+
+        else {
+            for (int cont = 1; cont < valor; cont ++) {
+                if( valor % cont == 0){
+                    total++;
+                    break;
+                }
+            }
+            if (total == 0){
+                System.out.println("Primo");
+            }
+            else {
+                System.out.println("Não é primo");
+            }
+        }
+
+
+    }
+}
---
+++ src/Teste.java
@@ -0,0 +1,9 @@
+public class Teste {
+
+    static void main() {
+
+    for (int cont =1; cont <= 10; cont++) {
+        System.out.println(cont);
+    }
+    }
+}
